51c3bfb1ea27f22a38378ee1590f3c095613ef62
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-08-30 10:11:44 +0600

    working on nav bar

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
-
-const Layout = () => {
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div className="layout">
+      <Head>
+        <title>Music Store</title>
+      </Head>
+      <header>
+        <Navbar>
+
+        </Navbar>
+      </header>
+      <main className="main-container">
+        {children}
+      </main>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
   )
 }
 
