42677726eb0fcbc79fda30479f21832361bb2b14
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-09-09 19:48:21 +0600

    working on cart section

--- context/StateContext.js
+++ context/StateContext.js
@@ -9,7 +9,7 @@ export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [totalPrice, setTotalPrice] = useState();
-    const [totalQuantities, setTotalQuantities] = useState();
+    const [totalQuantities, setTotalQuantities] = useState(0);
     const [qty, setQty] = useState(1);
 
     // Add to cart functionality
@@ -53,6 +53,7 @@ export const StateContext = ({ children }) => {
             value={{
                 showCart,
                 cartItems,
+                setShowCart,
                 totalPrice,
                 totalQuantities,
                 qty,
