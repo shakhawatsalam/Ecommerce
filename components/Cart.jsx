27e654048bc24605619a204a85690bbe537ad1e3
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-09-16 00:44:02 +0600

    whorking on payment method

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -10,14 +10,32 @@ import { TiDeleteOutline } from "react-icons/ti";
 import toast from "react-hot-toast";
 import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
+import getStripe from "../lib/getStripe";
 
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } =
     useStateContext();
+  console.log(cartItems);
 // Handle Checkout 
-  const handleCheckout = () => {
-    
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if (response.statusCode === 500) return;
+
+    const data = await response.json();
+
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({ sessionId: data.id });
   }
   return (
     <div className='cart-wrapper' ref={cartRef}>
